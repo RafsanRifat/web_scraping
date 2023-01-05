ef76fe76000469f03d83f5d55d64f37fb6e7e933
RafsanRifat <rafsan.rifat255@gmail.com>
2023-01-05 19:10:18 +0600

    data added in pandas

---
+++ weather_scraper.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.Y7a2fnZByUl"
+html_page = requests.get(url)
+
+soup = BeautifulSoup(html_page.content, 'html.parser')
+
+seven_day = soup.find(id='seven-day-forecast')
+
+tombstone_container = seven_day.find_all(class_="tombstone-container")
+tonight = tombstone_container[0]
+
+# getting data from tonight
+period = tonight.find(class_="period-name").get_text()
+short_desc = tonight.find(class_="short-desc").get_text()
+img = tonight.find('img', class_="forecast-icon")
+image = img['title']
+
+# print(period, short_desc, image)
+
+period_tags = seven_day.select(".tombstone-container .period-name")
+period = [pt.get_text() for pt in period_tags]
+# print(period)
+
+period = [pt.get_text() for pt in seven_day.select(".tombstone-container .period-name")]
+short_desc = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+image = [img['title'] for img in seven_day.select(".tombstone-container .forecast-icon")]
+
+# print(period)
+# print(short_desc)
+# print(image)
+
+import pandas as pd
+
+weather = pd.DataFrame({
+    "period": period,
+    "short_desc": short_desc,
+    "image": image
+})
+
+print(weather)
