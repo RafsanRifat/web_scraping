922c2de95b7dfa70528d465f173e89291c6acb99
RafsanRifat <rafsan.rifat255@gmail.com>
2023-01-30 18:49:33 +0600

    a new file added

--- imdb_scrapper.py
+++ imdb_scrapper.py
@@ -1,3 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+""" 
+Required data ------->>>
+ 
+"""
+
+url = "https://www.imdb.com/search/title/?release_date=2017&amp&sort=num_votes,desc&amp&page=1"
+html_data = requests.get(url)
+
+# html data into text
+html_data_into_text_form = html_data.text
+
+# parse text data using Beautifulsoup
+soup = BeautifulSoup(html_data_into_text_form, 'html.parser')
+
+# find movie div
+movie_containers = soup.find_all('div', class_='lister-item mode-advanced')
+print(type(movie_containers))
+print(len(movie_containers))
---
+++ test.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+url = "https://sendbird.com/"
+
+for _ in range(1000):
+    requests.get(url)
+    print("requesting --->>>")
+    print("requesting ------>>> (-_-)")
+    print("requesting -------->>> (-_-)")
+    print("requesting ---------->>> (-_-)")
+    print("requesting -------->>> (-_-)")
+    print("requesting ------->>> (-_-)")
+    print("requesting ---->>> (-_-)")
+    time.sleep(0.001/1000)
